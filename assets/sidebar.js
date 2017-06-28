1f87a54e6ae38d0e534e2771a8a50d8764b8e64c
Aditya Lahiri <aditya.lahiri2009@gmail.com>
2017-06-28 13:46:14 +0530

    Auto-generated commit

--- assets/sidebar.js
+++ assets/sidebar.js
@@ -1,7 +1,9 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    $('.glyph').delay('slow').hide();
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    $('.glyph').delay(500).fadeIn()
 }
