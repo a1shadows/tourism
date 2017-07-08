f4b7f34faa0e63beb02450b65170dff702481501
Aditya Lahiri <aditya.lahiri2009@gmail.com>
2017-07-08 14:53:02 +0530

    maps suggestions added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,56 @@
-import { Component } from '@angular/core';
+import {NgZone, OnInit, Component, ViewChild, ElementRef } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
+import { MapsAPILoader } from '@agm/core';
+//import { HttpJSONService } from './HttpJSON.service'
+
+import {} from '@types/googlemaps';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+    public searchControl: FormControl;
+    public lat : number;
+    public lon: number;
+
+    @ViewChild("search")
+    public searchElementRef: ElementRef;
+
+    constructor(
+      private mapsAPILoader: MapsAPILoader,
+      private ngZone: NgZone
+    ) {}
+
+    ngOnInit() {
+
+      //create search FormControl
+      this.searchControl = new FormControl();
+
+      //load Places Autocomplete
+      this.mapsAPILoader.load().then(() => {
+        let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        types: ["address"]
+      });
+      autocomplete.addListener("place_changed", () => {
+            this.ngZone.run(() => {
+              //get the place result
+              let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+              //verify result
+              if (place.geometry === undefined || place.geometry === null) {
+                return;
+              }
+
+              //set latitude, longitude and zoom
+              this.lat = place.geometry.location.lat();
+              this.lon = place.geometry.location.lng();
+              //getJSONrequest(this.lat, this.lon);
+        });
+      });
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import {OnInit, ViewChild, ElementRef, NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    HttpModule,
+    JsonpModule,
+    ReactiveFormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBs-mQp36I7YFqbVHapmk8lVS2bU9LZ5yI',
+      libraries: ["places"]
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
